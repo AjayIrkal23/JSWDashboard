6eaade74a7ef92d76029a4f3e36886a2d8bc1f66
ajayirkal23 <Ajayirkal.mercy@gmail.com>
2024-08-31 05:23:23 +0530

    PUSH

--- jswdasboard/src/components/Date.jsx
+++ jswdasboard/src/components/Date.jsx
@@ -39,35 +39,15 @@ const Date = ({ open, setOpen }) => {
         aria-describedby="modal-modal-description"
       >
         <div className="absolute  bg-white outline-none top-[25%] left-[50%] -translate-x-[50%] flex">
-          <div className="flex flex-col col-span-full sm:col-span-6 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+          <div className="flex flex-col bg-white border rounded-sm shadow-lg col-span-full sm:col-span-6 dark:bg-slate-800 border-slate-200 dark:border-slate-700">
             <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
               <h2 className="font-semibold text-slate-800 dark:text-slate-100">
                 Please Select Date
               </h2>
-              <div className="my-4 flex flex-col gap-4 w-full justify-center">
+              <div className="flex flex-col justify-center w-full gap-4 my-4">
                 <div>
                   <Datepicker setDateValue={setDateValue} />
                 </div>{" "}
-                <div>
-                  <h2 className="font-semibold text-slate-800 dark:text-slate-100 my-2">
-                    Start
-                  </h2>
-                  <TimePicker
-                    onChange={setStart}
-                    value={start}
-                    className="w-full"
-                  />
-                </div>
-                <div>
-                  <h2 className="font-semibold text-slate-800 dark:text-slate-100 my-2">
-                    End
-                  </h2>
-                  <TimePicker
-                    onChange={setEnd}
-                    value={end}
-                    className="w-full"
-                  />
-                </div>
               </div>
               <button
                 className="bg-blue-500 px-6 py-1.5 rounded-md flex justify-center w-full text-white mt-6"
